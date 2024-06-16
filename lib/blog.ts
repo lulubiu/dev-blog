41823cbd1058ecc8841b8e0b1eac6d6349069d6e
Developer.lulu <zhenglulu_0405@163.com>
2024-06-16 15:55:17 +0800

    change slug generate method

--- lib/blog.ts
+++ lib/blog.ts
@@ -3,6 +3,7 @@ import dayjs from 'dayjs';
 import fs from 'fs';
 import matter from 'gray-matter';
 import path from 'path';
+import { compareDesc } from 'date-fns';
 
 export async function getBlogPosts(): Promise<{ posts: BlogPost[]; postsByMonth: PostsByMonth }> {
   const postsDirectory = path.join(process.cwd(), 'content')
@@ -14,18 +15,22 @@ export async function getBlogPosts(): Promise<{ posts: BlogPost[]; postsByMonth:
       const fullPath = path.join(postsDirectory, filename)
       const fileContents = await fs.promises.readFile(fullPath, 'utf8')
 
+      const slug = filename.replace('.md', '')
       const { data, content } = matter(fileContents)
+      data.slug = slug
       const month = dayjs(data.date).format('YYYY-MM-DD').slice(0, 7);
 
       return {
         id: month,
         metadata: data, // slug/url title date
         title: data.title,
-        slug: data.slug,
+        slug: slug,
         content,
       }
     })
   )
+
+  posts.sort((a, b) => compareDesc(new Date(a.metadata.date), new Date(b.metadata.date)));
 
   // Group by month
   const postsByMonth: PostsByMonth = posts.reduce((acc: PostsByMonth, post: BlogPost) => {
