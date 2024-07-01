7f02360ddc913b9c321872eb1531cdbf0aa489ea
Developer.lulu <zhenglulu_0405@163.com>
2024-07-01 13:50:30 +0800

    udpate not-found page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,12 +1,15 @@
-import FollowButton from "@/components/FollowButton";
 import Image from "next/image";
 
 export default function NotFound() {
   return (
     <div className="mx-automax-h-screen flex flex-col items-center">
-      <Image src="/404.svg" alt="404" width={128} height={128} />
-      <div className="w-full text-center mt-8 flex flex-col sm:flex-row gap-2">
-        <FollowButton name="回到首页" href="/" target="_self"></FollowButton>
+      <Image src="/404.svg" alt="404" width={256} height={256} />
+      <div className="text-center mt-4 flex flex-col sm:flex-row">
+        <p>
+          <a href="https://lulubiu.com" className="text-slate-300 hover:text-sky-400">
+            回到首页
+          </a>
+        </p>
       </div>
     </div>
   );
