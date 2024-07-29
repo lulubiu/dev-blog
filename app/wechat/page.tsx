7a4542e70466e25974a329ec2b71a562f5ae74be
Developer.lulu <zhenglulu_0405@163.com>
2024-07-29 18:08:43 +0800

    add wechat page

---
+++ app/wechat/page.tsx
@@ -0,0 +1,16 @@
+export default function Coffee() {
+  return (
+    <div className="mx-automax-h-screen flex flex-col items-center">
+        <img src="wechat.jpg" alt="赞赏码" width="25%" height="25%"/>
+        <div className="text-center mt-4 flex flex-col sm:flex-row">
+            <ul className="mt-4 space-y-2">
+                <li>
+                    <a href="/" className="block h-full rounded-lg border border-gray-700 p-4 hover:border-pink-600">
+                        <strong className="font-medium text-white">感谢您的赞赏，今日份超大杯开心</strong>
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+  );
+}
