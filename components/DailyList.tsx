55f388cd067b44dda5395fadc4b4ca647d0dafff
Developer.lulu <zhenglulu_0405@163.com>
2024-07-31 22:39:42 +0800

    refactor daily section

---
+++ components/DailyList.tsx
@@ -0,0 +1,37 @@
+import { BlogPost } from "@/types/blog";
+import dayjs from "dayjs";
+// import Link from "next/link";
+import { Link } from "next-view-transitions";
+
+export default async function DailyList({
+  isSide,
+  posts,
+}: {
+  isSide?: boolean;
+  posts: BlogPost[];
+}) {
+  return (
+    <ul className="flex flex-col gap-4">
+      {posts.map((post) => (
+        <li
+          key={post.slug}
+          className="flex flex-col sm:flex-row gap-4 items-start"
+        >
+          {isSide ? (
+            <></>
+          ) : (
+            <span className="text-[#8585a8] min-w-28">
+              {dayjs(post.metadata.date).format("YYYY-MM-DD")}
+            </span>
+          )}
+          <Link
+            href={`/daily/${post.slug}`}
+            className="link-default truncate transition-colors duration-500 ease-in-out"
+          >
+            {post.title}
+          </Link>
+        </li>
+      ))}
+    </ul>
+  );
+}
---
+++ lib/daily.ts
@@ -0,0 +1,34 @@
+import { BlogPost } from '@/types/blog';
+import { compareDesc } from 'date-fns';
+import fs from 'fs';
+import matter from 'gray-matter';
+import path from 'path';
+
+export async function getDailyPosts(): Promise<{ posts: BlogPost[]}> {
+  const postsDirectory = path.join(process.cwd(), 'content/daily')
+  let filenames = await fs.promises.readdir(postsDirectory)
+
+  const posts = await Promise.all(
+    filenames.map(async (filename) => {
+      const fullPath = path.join(postsDirectory, filename)
+      const fileContents = await fs.promises.readFile(fullPath, 'utf8')
+
+      const slug = filename.replace('.md', '')
+      const { data, content } = matter(fileContents)
+      data.slug = slug
+
+      return {
+        metadata: data,
+        title: data.title,
+        slug: slug,
+        content,
+      }
+    })
+  )
+
+  posts.sort((a, b) => compareDesc(new Date(a.metadata.date), new Date(b.metadata.date)));
+
+  return {
+    posts
+  }
+}
