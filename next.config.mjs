cc4ac085fe8f41ec8be2d1e22f4f5a54b3b2a0eb
Developer.lulu <zhenglulu_0405@163.com>
2024-09-03 19:54:52 +0800

    enable optimizecss

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,9 @@ import remarkGfm from "remark-gfm";
 const nextConfig = {
   // Configure `pageExtensions`` to include MDX files
   pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
+  experimental: {
+    optimizeCss: true,
+  },
   // Optionally, add any other Next.js config below
   output: "export"
 };
